a4b2c38c02506c8432075bc2588cf9a2b5ac6801
necatiozmen$ git config --global user.email necati.ozmen.9@gmail.comgit configgit config --global user.name necatiozmen <necati.ozmen.9@gmail.com>
2018-10-08 17:49:27 +0300

    feat:Add proptypes for Congrats

---
+++ src/components/Congrats/Congrats.test.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import Enzyme, { shallow } from 'enzyme';
+import EnzymeAdapter from 'enzyme-adapter-react-16';
+import Congrats from './Congrats';
+import { findByTestAttr, checkProps } from '../../../test/testUtils';
+
+Enzyme.configure({ adapter: new EnzymeAdapter() });
+
+const defaultProps = { success: false };
+/**
+ * Factory function to create a ShalowWrapper for the Congrats component.
+ * @function setup
+ * @param {object} props - Component props specific to this setup.
+ * @returns {ShallowWrapper}
+*/
+
+const setup = (props={}) => {
+  //gelen props u default uzerine yazdik
+  const setupProps = { ...defaultProps, ...props};
+  return shallow(<Congrats {...setupProps} />)
+}
+
+test('renders without error', () => {
+  const wrapper = setup();
+  const component = findByTestAttr(wrapper, 'component-congrats');
+  expect(component.length).toBe(1);
+});
+
+test('renders no text when `success` props is false ', () => {
+  const wrapper = setup({ success: false });
+  const component = findByTestAttr(wrapper, 'component-congrats');
+  expect(component.text()).toBe('');
+});
+
+test('renders non-empty congrats message when `success` prop is true', () => {
+  const wrapper = setup({ success: true });
+  const message = findByTestAttr(wrapper, 'congrats-message');
+  expect(message.text().length).not.toBe(0);
+});
+//sanirim proptypes testlerinden basarili gecerse hata vermemesi icin kullandik
+test('does not throw warning with expected props', () => {
+  const expectedProps = { success:false };
+  checkProps(Congrats,expectedProps);
+})
