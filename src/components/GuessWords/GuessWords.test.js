ec1853c6ad068d43e2b8a0ae825c51b77d58f818
necatiozmen$ git config --global user.email necati.ozmen.9@gmail.comgit configgit config --global user.name necatiozmen <necati.ozmen.9@gmail.com>
2018-10-08 22:02:59 +0300

    feat:Test and code for non-zero-test

--- src/components/GuessWords/GuessWords.test.js
+++ src/components/GuessWords/GuessWords.test.js
@@ -26,24 +26,44 @@ test('does not throw warning with expected props', () => {
 
 describe('if there are no words guessed', () => {
   //her test icinde ortak kullanilan methidlar oldugu icin beforeEach kullanildi
-  let wrapper
+  let wrapper;
   beforeEach(() => {
     wrapper = setup({ guessedWords: [] });
   });
 
   test('renders without error', () => {
     const component = findByTestAttr(wrapper, 'component-guessed-words');
-    // console.log('component.length', component.length);
     expect(component.length).toBe(1);
   });
 
   test('renders instructions to guess a word',() => {
    const instructions = findByTestAttr(wrapper,'guess-instructions');
    expect(instructions.text().length).not.toBe(0);
-
   })
 });
 
 describe('if there are words guessed', () => {
+  let wrapper;
+  const guessedWords = [
+    { guessedWord: 'train', letterMatchCount:3 },
+    { guessedWord: 'agile', letterMatchCount:1 },
+    { guessedWord: 'party', letterMatchCount:5 }
+  ];
 
+  beforeEach(() => {
+    wrapper = setup({ guessedWords });
+  });
+
+  test('renders without error', () => {
+    const component = findByTestAttr(wrapper, 'component-guessed-words');
+    expect(component.length).toBe(1);
+  });
+  test('renders "guessed words" section', () => {
+    const guessedWordsNode = findByTestAttr(wrapper, 'guessed-words');
+    expect(guessedWordsNode.length).toBe(1);
+  });
+  test('correct number of guessed words', () => {
+    const guessedWordNodes = findByTestAttr(wrapper, 'guessed-word');
+    expect(guessedWordNodes.length).toBe(guessedWords.length);
+  });
 });
