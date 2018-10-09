d1ae205e04f34259bb5745780a95961fe3129046
necatiozmen$ git config --global user.email necati.ozmen.9@gmail.comgit configgit config --global user.name necatiozmen <necati.ozmen.9@gmail.com>
2018-10-09 13:25:18 +0300

    feat:Setup Input component and storefactory

--- test/testUtils.js
+++ test/testUtils.js
@@ -1,4 +1,17 @@
 import checkPropTypes from 'check-prop-types';
+import {createStore} from 'redux';
+
+import rootReducer from '../src/reducers';
+
+/**
+ * Create a testing store with imported reducers, middleware, abd initial state
+ * @param  {object} initialState initial state for store
+ * @function storeFactory
+ * @return {Store} - Redux store
+ */
+export const storeFactory = (initialState) => {
+  return createStore(rootReducer, initialState);
+}
 
 /**
 * Return ShallowWrapper containing nodes with the given data-test value
