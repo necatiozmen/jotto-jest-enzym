b96246264818f5f1e040aaf6a53617fb816a9093
necatiozmen$ git config --global user.email necati.ozmen.9@gmail.comgit configgit config --global user.name necatiozmen <necati.ozmen.9@gmail.com>
2018-10-09 19:32:45 +0300

    feat:Redux-thunk setup

--- test/testUtils.js
+++ test/testUtils.js
@@ -1,7 +1,8 @@
 import checkPropTypes from 'check-prop-types';
-import {createStore} from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 
 import rootReducer from '../src/reducers';
+import { middlewares } from '../src/configureStore';
 
 /**
  * Create a testing store with imported reducers, middleware, abd initial state
@@ -11,7 +12,8 @@ import rootReducer from '../src/reducers';
  */
 
 export const storeFactory = (initialState) => {
-  return createStore(rootReducer, initialState);
+  const createStoreWithMiddleware = applyMiddleware(...middlewares)(createStore)
+  return createStoreWithMiddleware(rootReducer, initialState);
 }
 
 /**
